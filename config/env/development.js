b1687a9985ab3c4bfba5cfdea3e4e10983cfd2df
Kyle Hill <kylehill@gmail.com>
2015-04-03 16:06:50 -0400

    Bumps a line from development

--- config/env/development.js
+++ config/env/development.js
@@ -12,13 +12,5 @@
 
 module.exports = {
 
-  /***************************************************************************
-   * Set the default database connection for models in the development       *
-   * environment (see config/connections.js and config/models.js )           *
-   ***************************************************************************/
-
-  models: {
-    connection: 'local'
-  }
-
+ 
 };
